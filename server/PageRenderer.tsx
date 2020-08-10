ae70d930ef9f5b9d98c4367ae95966920aa60129
Kyle Flynn <kyle.flynn5798@yahoo.com>
2020-08-09 21:29:31 -0400

    Server-rendered content works!

--- server/PageRenderer.tsx
+++ server/PageRenderer.tsx
@@ -2,18 +2,31 @@ import * as React from "react";
 import { Request } from "express";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
-import { getHomeData, IApplicationState, IApplicationStateJSON, IHomeProps, defaultState, Reducer } from "shared";
+import {
+  getHomeData,
+  IApplicationState,
+  IApplicationStateJSON,
+  IHomeProps,
+  defaultState,
+  Reducer,
+  ITeamsProps,
+  getTeamsData,
+  IEventsProps,
+  getEventsData
+} from "shared";
 import App from "../client/App";
 import Event from "@the-orange-alliance/api/lib/models/Event";
 import Team from "@the-orange-alliance/api/lib/models/Team";
 import Match from "@the-orange-alliance/api/lib/models/Match";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import MatchParticipant from "@the-orange-alliance/api/lib/models/MatchParticipant";
 
 export async function render(req: Request, file: Buffer): Promise<string> {
   const initialState: IApplicationState = await loadPageData(req, req.params);
   const store = createStore(Reducer, initialState);
   const state: IApplicationState = store.getState();
+  const context: any = {};
 
   const app = React.createElement(App, { store: store });
   const router = React.createElement(StaticRouter, { location: req.url, context: context }, app);
@@ -36,9 +49,9 @@ function prepareState(state: IApplicationState): IApplicationStateJSON {
     teams: state.teams.map((t: Team) => t.toJSON()),
     matches: state.matches.map((m: Match) => m.toJSON()),
     highScoreMatches: {
-      elims: state.highScoreMatches.elims.toJSON(),
-      overall: state.highScoreMatches.overall.toJSON(),
-      quals: state.highScoreMatches.quals.toJSON()
+      elims: convertFullMatchToJSON(state.highScoreMatches.elims),
+      overall: convertFullMatchToJSON(state.highScoreMatches.overall),
+      quals: convertFullMatchToJSON(state.highScoreMatches.quals)
     }
   };
 }
@@ -46,7 +59,7 @@ function prepareState(state: IApplicationState): IApplicationStateJSON {
 async function loadPageData(req: any, params?: any): Promise<IApplicationState> {
   switch (req.path) {
     case "/":
-      const defaultProps: IHomeProps = {
+      const defaultHomeProps: IHomeProps = {
         eventSize: 0,
         teamSize: 0,
         highScoreMatches: {
@@ -55,14 +68,36 @@ async function loadPageData(req: any, params?: any): Promise<IApplicationState> 
           elims: new Match()
         }
       };
-      const props: IHomeProps = await getHomeData(defaultProps);
+      const homeProps: IHomeProps = await getHomeData(defaultHomeProps);
       return {
         ...defaultState,
-        eventsTotal: props.eventSize,
-        teamsTotal: props.teamSize,
-        highScoreMatches: props.highScoreMatches
+        eventsTotal: homeProps.eventSize,
+        teamsTotal: homeProps.teamSize,
+        highScoreMatches: homeProps.highScoreMatches
+      };
+    case "/teams":
+      const defaultTeamsProps: ITeamsProps = { teams: [] };
+      const teamsProps: ITeamsProps = await getTeamsData(defaultTeamsProps);
+      return {
+        ...defaultState,
+        teams: teamsProps.teams
+      };
+    case "/events":
+      const defaultEventsProps: IEventsProps = { events: [] };
+      const eventsProps: IEventsProps = await getEventsData(defaultEventsProps);
+      return {
+        ...defaultState,
+        events: eventsProps.events
       };
     default:
       return defaultState;
   }
+}
+
+function convertFullMatchToJSON(match: Match): any {
+  return {
+    ...match.toJSON(),
+    participants: match.participants.map((p: MatchParticipant) => p.toJSON()),
+    event: match.event.toJSON()
+  };
 }
