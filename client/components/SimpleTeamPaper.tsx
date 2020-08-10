3b344fdb237410ac298a3c06678eb04b1cb4c46b
Josh Bacon <bacon.josh09@gmail.com>
2020-08-10 12:53:42 -0400

    Convert all current pages over to i18n format with hooks

--- client/components/SimpleTeamPaper.tsx
+++ client/components/SimpleTeamPaper.tsx
@@ -1,25 +1,19 @@
-import * as React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar/Avatar';
+import * as React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar/Avatar";
 
-class SimpleTeamPaper extends React.Component {
-  public constructor(props: any) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <ListItem button>
-        <ListItemAvatar>
-          <Avatar>FIM</Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={'Team #33'} secondary={'Killer Bees'} />
-      </ListItem>
-    );
-  }
-}
+const SimpleTeamPaper = function () {
+  return (
+    <ListItem button>
+      <ListItemAvatar>
+        <Avatar>FIM</Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={"Team #33"} secondary={"Killer Bees"} />
+    </ListItem>
+  );
+};
 
 export default SimpleTeamPaper;
--- client/pages/events/EventsPage.tsx
+++ client/pages/events/EventsPage.tsx
@@ -1,49 +1,26 @@
-import * as React from 'react';
-import Event from '@the-orange-alliance/api/lib/models/Event';
-import {
-  IApplicationState,
-  IEventsProps,
-  getEventsData,
-  ISetEvents,
-  ApplicationActions,
-  setEvents
-} from 'shared';
-import { Dispatch } from 'redux';
-import { connect } from 'react-redux';
+import * as React from "react";
+import Event from "@the-orange-alliance/api/lib/models/Event";
+import { IApplicationState, IEventsProps, getEventsData, ISetEvents, ApplicationActions, setEvents } from "shared";
+import { Dispatch } from "redux";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 interface IProps {
   events: Event[];
   setEvents: (events: Event[]) => ISetEvents;
 }
 
-class EventsPage extends React.Component<IProps> {
-  public constructor(props: IProps) {
-    super(props);
-  }
-
-  public componentDidMount(): void {
-    const { events, setEvents } = this.props;
-
+const EventsPage = function () {
+  const { t } = useTranslation();
+  const events = useSelector((state: IApplicationState) => state.events);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
     getEventsData({ events }).then((props: IEventsProps) => {
-      setEvents(props.events);
+      dispatch(setEvents(props.events));
     });
-  }
+  }, []);
 
-  public render() {
-    return <div>Hello World!</div>;
-  }
-}
+  return <div>{t("pages.events.title")}</div>;
+};
 
-function mapStateToProps(state: IApplicationState) {
-  return {
-    events: state.events
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch<ApplicationActions>) {
-  return {
-    setEvents: (events: Event[]) => dispatch(setEvents(events))
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(EventsPage);
+export default EventsPage;
