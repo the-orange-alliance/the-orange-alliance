c48a88e8213f22b6db439bfdcef94d901fbf1a9d
Josh Bacon <bacon.josh09@gmail.com>
2022-11-07 22:25:07 +0000

    Fix builds with react 18

--- components/SimpleEventPaper.tsx
+++ components/SimpleEventPaper.tsx
@@ -2,30 +2,16 @@ import * as React from 'react';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import { Event } from '@the-orange-alliance/api/lib/cjs/models';
-import { makeStyles } from '@mui/styles';
-import { Theme } from '@mui/material/styles';
 import { readableDate } from '../lib/utils/common';
 import { useRouter } from 'next/router';
+import { Box } from '@mui/system';
 
 interface IProps {
   event: Event;
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  dataIndicator: {
-    background: theme.palette.primary.main,
-    top: '4px',
-    left: '4px',
-    width: '4px',
-    bottom: '4px',
-    position: 'absolute',
-    borderRadius: '4px'
-  }
-}));
-
 const SimpleEventPaper = (props: IProps) => {
   const { event } = props;
-  const classes = useStyles();
   const router = useRouter();
 
   const secondaryTxt =
@@ -45,7 +31,15 @@ const SimpleEventPaper = (props: IProps) => {
 
   return (
     <ListItem button onClick={onClick}>
-      {event.teamCount > 0 && <div className={classes.dataIndicator}></div>}
+      {event.teamCount > 0 && <Box sx={{
+        background: "primary.main",
+        top: '4px',
+        left: '4px',
+        width: '4px',
+        bottom: '4px',
+        position: 'absolute',
+        borderRadius: '4px'
+      }}></Box>}
       <ListItemText primary={event.fullEventName} secondary={secondaryTxt} />
     </ListItem>
   );
