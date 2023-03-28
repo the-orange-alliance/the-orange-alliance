2b685f7758c15c6e5450678332d6379831226fd7
Ofek Ashery <ofek.ashery@gmail.com>
2023-03-28 13:39:54 +0300

    Redesign match details

--- components/MatchDetails/MatchDetailsCard.tsx
+++ components/MatchDetails/MatchDetailsCard.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
 import Match from '@the-orange-alliance/api/lib/cjs/models/Match';
 import MatchBreakdown1617 from './MatchBreakdowns/MatchBreakdown1617';
 import MatchBreakdown1718 from './MatchBreakdowns/MatchBreakdown1718';
@@ -7,100 +9,131 @@ import MatchBreakdown2021 from './MatchBreakdowns/MatchBreakdown2021';
 import MatchBreakdown2122 from './MatchBreakdowns/MatchBreakdown2122';
 import MatchBreakdown2223 from './MatchBreakdowns/MatchBreakdown2223';
 import { MatchBreakdownRow, MatchBreakdownTitle } from './MatchBreakdownRow';
-import { Box, Divider, Grid, Typography } from '@mui/material';
 
-interface IProps {
+interface MatchDetailsCardProps {
   match: Match;
 }
-const MatchDetailsCard = (props: IProps) => {
-  const { match } = props;
 
-  let rows: MatchBreakdownRow[] = [];
+const MatchDetailsCard: React.FC<MatchDetailsCardProps> = ({ match }) => {
+  const isRemote = match.redScore > -1 && match.blueScore === -1;
 
-  if (match.details) {
-    switch (parseInt(match.matchKey.split('-')[0], 10)) {
-      case 1617:
-        rows = new MatchBreakdown1617().getRows(match);
-        break;
-      case 1718:
-        rows = new MatchBreakdown1718().getRows(match);
-        break;
-      case 1819:
-        rows = new MatchBreakdown1819().getRows(match);
-        break;
-      case 1920:
-        rows = new MatchBreakdown1920().getRows(match);
-        break;
-      case 2021:
-        rows = new MatchBreakdown2021().getRows(match);
-        break;
-      case 2122:
-        rows = new MatchBreakdown2122().getRows(match);
-        break;
-      case 2223:
-        rows = new MatchBreakdown2223().getRows(match);
-        break;
-      default:
-        rows = [
-          MatchBreakdownTitle('Autonomous', match.redAutoScore, match.blueAutoScore),
-          MatchBreakdownTitle('Teleop', match.redTeleScore, match.blueTeleScore),
-          MatchBreakdownTitle('End Game', match.redEndScore, match.blueEndScore),
-          MatchBreakdownTitle('Penalty', match.bluePenalty, match.redPenalty)
-        ];
-        break;
+  let rows: MatchBreakdownRow[] = useMemo(() => {
+    if (match.details) {
+      switch (parseInt(match.matchKey.split('-')[0], 10)) {
+        case 1617:
+          return new MatchBreakdown1617().getRows(match);
+        case 1718:
+          return new MatchBreakdown1718().getRows(match);
+        case 1819:
+          return new MatchBreakdown1819().getRows(match);
+        case 1920:
+          return new MatchBreakdown1920().getRows(match);
+        case 2021:
+          return new MatchBreakdown2021().getRows(match);
+        case 2122:
+          return new MatchBreakdown2122().getRows(match);
+        case 2223:
+          return new MatchBreakdown2223().getRows(match);
+      }
     }
-  } else if (match.blueAutoScore > -1) {
-    rows = [
-      MatchBreakdownTitle('Autonomous', match.redAutoScore, match.blueAutoScore),
-      MatchBreakdownTitle('Teleop', match.redTeleScore, match.blueTeleScore),
-      MatchBreakdownTitle('End Game', match.redEndScore, match.blueEndScore),
-      MatchBreakdownTitle('Penalty', match.bluePenalty, match.redPenalty)
-    ];
-  }
+
+    if (match.redAutoScore > -1) {
+      return [
+        MatchBreakdownTitle('Autonomous', match.redAutoScore, match.blueAutoScore),
+        MatchBreakdownTitle('Teleop', match.redTeleScore, match.blueTeleScore),
+        MatchBreakdownTitle('End Game', match.redEndScore, match.blueEndScore),
+        MatchBreakdownTitle('Penalty', match.bluePenalty, match.redPenalty)
+      ];
+    } else {
+      return [];
+    }
+  }, [match]);
 
   return rows.length > 0 ? (
     <Box>
-      {rows.map(r => [
-        <Grid key={r.name + r.isTitle + r.red + r.blue + '-row'} container>
+      <Grid container>
+        <Grid xs={6} item />
+        <Grid
+          xs={isRemote ? 6 : 3}
+          sx={{
+            p: 1,
+            color: '#fff',
+            bgcolor: isRemote ? 'var(--toa-colors-tie)' : 'var(--toa-colors-red)',
+            fontSize: '1.125rem',
+            fontWeight: 700,
+            textAlign: 'center',
+            lineHeight: 1.25
+          }}
+          item
+        >
+          {!isRemote && (
+            <span style={{ display: 'block', fontSize: '0.75em', fontWeight: 500 }}>Red</span>
+          )}{' '}
+          {match.redScore}
+        </Grid>
+        {!isRemote && (
+          <Grid
+            xs={3}
+            sx={{
+              p: 1,
+              color: '#fff',
+              bgcolor: 'var(--toa-colors-blue)',
+              fontSize: '1.125rem',
+              fontWeight: 700,
+              textAlign: 'center',
+              lineHeight: 1.25
+            }}
+            item
+          >
+            <span style={{ display: 'block', fontSize: '0.75em', fontWeight: 500 }}>Blue</span>
+            {match.blueScore}
+          </Grid>
+        )}
+      </Grid>
+      {rows.map((r, index) => (
+        <Grid
+          key={index}
+          container
+          sx={{ backgroundColor: r.isTitle ? 'rgba(0, 0, 0, 0.04)' : '' }}
+        >
+          <Grid xs={6} sx={{ padding: 1 }} item>
+            <Typography style={{ fontWeight: r.isTitle ? 'bold' : 'normal' }}>{r.name}</Typography>
+          </Grid>
+
           {/* Red Details */}
           <Grid
-            xs={4}
+            xs={isRemote ? 6 : 3}
             sx={{
               padding: 1,
-              backgroundColor: 'var(--toa-colors-red-transparent)',
+              backgroundColor: isRemote
+                ? 'var(--toa-colors-tie-transparent)'
+                : 'var(--toa-colors-red-transparent)',
               textAlign: 'center'
             }}
             item
           >
             <Typography
-              variant={'body1'}
-              display={'inline-flex'}
-              style={{ fontWeight: r.isTitle ? 'bold' : 'normal', verticalAlign: 'bottom' }}
+              display="inline-flex"
+              style={{
+                verticalAlign: 'bottom',
+                color:
+                  r.red !== 0
+                    ? isRemote
+                      ? 'var(--toa-colors-tie)'
+                      : 'var(--toa-colors-red)'
+                    : 'rgba(0, 0, 0, 0.54)',
+                fontWeight: r.isTitle ? 700 : r.red !== 0 ? 500 : 400
+              }}
             >
               {r.redIcon}
               {r.getRedPoints()}
             </Typography>
           </Grid>
 
-          {/* Blue Details */}
-          <Grid
-            xs={4}
-            sx={{ padding: 1, backgroundColor: r.isTitle ? 'rgb(240, 240, 240)' : '' }}
-            item
-          >
-            <Typography
-              variant={'body1'}
-              align={'center'}
-              style={{ fontWeight: r.isTitle ? 'bold' : 'normal' }}
-            >
-              {r.name}
-            </Typography>
-          </Grid>
-
           {/* Blue Details if we have a blue score (not a single-team match) */}
-          {match.blueScore !== -1 && (
+          {!isRemote && (
             <Grid
-              xs={4}
+              xs={3}
               sx={{
                 padding: 1,
                 backgroundColor: 'var(--toa-colors-blue-transparent)',
@@ -109,21 +142,20 @@ const MatchDetailsCard = (props: IProps) => {
               item
             >
               <Typography
-                variant={'body1'}
-                display={'inline-flex'}
-                style={{ fontWeight: r.isTitle ? 'bold' : 'normal', verticalAlign: 'bottom' }}
+                display="inline-flex"
+                style={{
+                  verticalAlign: 'bottom',
+                  color: r.blue !== 0 ? 'var(--toa-colors-blue)' : 'rgba(0, 0, 0, 0.54)',
+                  fontWeight: r.isTitle ? 700 : r.blue !== 0 ? 500 : 400
+                }}
               >
                 {r.blueIcon}
                 {r.getBluePoints()}
               </Typography>
             </Grid>
           )}
-        </Grid>,
-        <Divider
-          key={r.name + r.isTitle + r.red + r.blue + '-divider'}
-          style={{ opacity: '10%' }}
-        />
-      ])}
+        </Grid>
+      ))}
     </Box>
   ) : null;
 };
