d9f720e0be61df605aa26cf8eb1524fe21b4a259
Ofek Ashery <ofek.ashery@gmail.com>
2025-04-19 13:33:15 +0300

    Refactor alliances tab

--- components/pages/event/AlliancesTab.tsx
+++ components/pages/event/AlliancesTab.tsx
@@ -1,4 +1,5 @@
-import { Table, TableCell, TableHead, TableBody, TableRow } from '@mui/material';
+import NextLink from 'next/link';
+import { Table, TableCell, TableHead, TableBody, TableRow, Link } from '@mui/material';
 import { Alliance, Event } from '@the-orange-alliance/api/lib/cjs/models';
 
 interface IProps {
@@ -7,24 +8,45 @@ interface IProps {
 
 const AlliancesTab = (props: IProps) => {
   const { alliances } = props.event;
+  const hasPick2 = alliances.some((alliance: Alliance) => alliance.pick2);
 
   return (
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell>Alliance</TableCell>
+          <TableCell>Seed</TableCell>
           <TableCell>Captain</TableCell>
           <TableCell>Pick 1</TableCell>
-          <TableCell>Pick 2</TableCell>
+          {hasPick2 && <TableCell>Pick 2</TableCell>}{' '}
         </TableRow>
       </TableHead>
       <TableBody>
         {alliances.map((alliance: Alliance) => (
           <TableRow key={alliance.seed}>
-            <TableCell>Alliance {alliance.seed}</TableCell>
-            <TableCell>#{alliance.captain.teamKey}</TableCell>
-            <TableCell>#{alliance.pick1.teamKey}</TableCell>
-            <TableCell>#{alliance.pick2 ? alliance.pick2.teamKey : ''}</TableCell>
+            <TableCell style={{ whiteSpace: 'nowrap' }}>Alliance {alliance.seed}</TableCell>
+            <TableCell>
+              <NextLink href={`/teams/${alliance.captain.teamKey}`} passHref legacyBehavior>
+                <Link underline="hover">
+                  #{alliance.captain.teamKey} {alliance.captain.teamNameShort}
+                </Link>
+              </NextLink>
+            </TableCell>
+            <TableCell>
+              <NextLink href={`/teams/${alliance.pick1.teamKey}`} passHref legacyBehavior>
+                <Link underline="hover">
+                  #{alliance.pick1.teamKey} {alliance.pick1.teamNameShort}
+                </Link>
+              </NextLink>
+            </TableCell>
+            {hasPick2 && (
+              <TableCell>
+                <NextLink href={`/teams/${alliance.pick2!.teamKey}`} passHref legacyBehavior>
+                  <Link underline="hover">
+                    #{alliance.pick2!.teamKey} {alliance.pick2!.teamNameShort}
+                  </Link>
+                </NextLink>
+              </TableCell>
+            )}
           </TableRow>
         ))}
       </TableBody>
